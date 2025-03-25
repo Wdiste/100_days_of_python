0325ef74932d12a9a26c448a702512b5580d515e
wdiste <Wdiste49@gmail.com>
2025-03-25 14:48:48 -0400

    Day 6 completed with reeborg maze

---
+++ Day_6/notes.py
@@ -0,0 +1,25 @@
+## defining a function:
+
+## use def keyword
+
+def my_function():
+    print("my function is operational")
+    print("exiting my function")
+
+my_function()
+
+## blocks of code are defined by indentation, similar to {} in JS
+
+# def indentation():
+#     if value_1 == true:
+#         if value_2 == false:
+
+
+# while loops work until something changes
+
+while distance_left > 0:
+    run()
+
+# this will make your character run until theyve reached the finish line
+# given that distance left and run are defined
+
