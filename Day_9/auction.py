06e533bfad6563306cc0490ce8f6e4a2a03dc60e
wdiste <Wdiste49@gmail.com>
2025-03-31 17:06:16 -0400

    auction functionality fully working,  All bidders added to dictionary and highest bid calculated correctly

--- Day_9/auction.py
+++ Day_9/auction.py
@@ -9,39 +9,45 @@ bidders = {
     "highest_bid" : ["none", 0]
 }
 
-def auction():
-    name = input("What is your name?")
-    current_bid = int(input("How much would you like to bid?"))
-    bidders[name] = current_bid
+def auction(more_bids):
+    while more_bids == True:
+        name = input("What is your name?  ")
+        current_bid = int(input("How much would you like to bid?  ")) # forcing an int because input is interpreted as str
+        bidders[name] = current_bid
 
-    if current_bid > bidders["highest_bid"][1]:
-        bidders["highest_bid"] = [name, current_bid]
-    
+        if current_bid > bidders["highest_bid"][1]:
+            bidders["highest_bid"] = [name, current_bid]
+        
+        if input("Would you like to make another bid? (Y/N)  ").lower() == "n":
+            more_bids = False
 
-
-auction()
-print(bidders)
-
-
-
-# print("""                         ___________
-#                          \\         /
-#                           )_______(
-#     SILENT                |"""""""|_.-._,.---------.,_.-._
-#        AUCTION            |       | | |               | | ''-.
-#                           |       |_| |_             _| |_..-'
-#                           |_______| '-' `'---------'` '-'
-#                           )"""""""(
-#                          /_________\\
-#                          `'-------'`
-#                        .-------------.
-#                    jgs/_______________\\""")
+    print(f"Congratulations {bidders['highest_bid'][0]}, you are the winner!")
+    print(f"{bidders['highest_bid'][0]}, please pay your bill of ${bidders['highest_bid'][1]} at our kiosk.")
 
 
+print("""                         ___________
+                         \\         /
+                          )_______(
+    SILENT                |"""""""|_.-._,.---------.,_.-._
+       AUCTION            |       | | |               | | ''-.
+                          |       |_| |_             _| |_..-'
+                          |_______| '-' `'---------'` '-'
+                          )"""""""(
+                         /_________\\
+                         `'-------'`
+                       .-------------.
+                   jgs/_______________\\""")
+if input("Ready to start? (Y/N)  ").lower() == "y":
+    print("Lets meet our first bidder!")
+    auction(more_bids = True)
+else:
+    print("Goodbye")
 
 
 
-# if input("Ready to start? (Y/N)") == "y" or "Y":
-#     print("")
-# else:
-#     print("Goodbye")
+
+
+
+
+
+
