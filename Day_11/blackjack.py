4cdbac5d219145cc24ce5a6042c468fd50c7be93
wdiste <Wdiste49@gmail.com>
2025-04-16 18:04:35 -0400

    Final project meets mvp. Ready to turn in

--- Day_11/blackjack.py
+++ Day_11/blackjack.py
@@ -25,7 +25,8 @@ def blackjack():
         }
     player_hand = [0]
     dealer_hand = [0]
-    stand = False
+    player_stand = False
+    dealer_stand = False
     
     #############################################################################
     def hand_value(hand, deck):
@@ -47,7 +48,7 @@ def blackjack():
         else:
             return value
     #############################################################################
-
+    ####    Initial draw phase
     if input("Would you like to play blackjack? (Y/N) 🂡 🂮 ").lower() == "y":
         print("Let's Play!\n\n")
         for i in range(2):
@@ -59,22 +60,46 @@ def blackjack():
         player_hand[0] = "Goodbye"
         print("Goodbye")
         return
-       
-    while stand == False and type(player_hand[0]) == int:
+    ####     End Draw Phase
+    ####     PLAYERS TURN   
+    while player_stand == False and type(player_hand[0]) == int:
         print(f"Dealer: {dealer_hand[1]}, 🂠\nPlayer: {player_hand[1:]} Total: {player_hand[0]}")
-        hit_or_stand = input("Would you like another card?").lower()
+        hit_or_stand = input("\nWould you like another card?").lower()
         if hit_or_stand == "n":
-            stand = True
+            player_stand = True
         elif hit_or_stand == "y":
             player_hand.append(random.choice(list(deck)))
             player_hand[0] = hand_value(player_hand, deck)
-
-    print(f"Dealer: {dealer_hand[1:]}\nPlayer: {player_hand[1:]} Total: {player_hand[0]}")
-
+    ####     END PLAYERS TURN
+    ####    DEALERS TURN
+    while dealer_stand == False and type(dealer_hand[0]) == int:
+        if dealer_hand[0] < 17:
+            print("\nThe dealer draws a card!")
+            dealer_hand.append(random.choice(list(deck)))
+            dealer_hand[0] = hand_value(dealer_hand, deck)
+            print(f"Dealer: {dealer_hand[1]}, {"🂠, " * (len(dealer_hand) - 2)}\nPlayer: {player_hand[1:]} Total: {player_hand[0]}")
+        else:
+            dealer_stand = True
+    ####    END DEALERS TURN
+    print(f"\n\n\nPlayer: {player_hand[1:]} Total: {player_hand[0]}")
     if player_hand[0] == "Blackjack!":
         print("\n\nCongrats! Player wins!")
+        return
     elif player_hand[0] == "Bust":
-        print("\n\nDealer wins! Better luck next time!")
-    return 0
+        print("\n\nPlayer busts! Better luck next time!")
+        return
+
+    print(f"Dealer: {dealer_hand[1:]} Total: {dealer_hand[0]}\n\n")
+    if dealer_hand[0] == "Blackjack!":
+        print("\n\nDealer has Blackjack! Dealer wins!")
+    elif dealer_hand[0] == "Bust":
+        print("\n\nDealer busts! Player Wins!")
+    elif player_hand[0] == dealer_hand[0]:
+        print("Push!")
+
+    if input("\nPlay again? (Y/N)").lower() == "y":
+        blackjack()
+    else:
+        return 0
 
 blackjack()
